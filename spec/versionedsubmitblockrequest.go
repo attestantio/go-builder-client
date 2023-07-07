d998368cb6d8521cb51564af6d08d0f2fb5cfec0
avalonche <avalonche@protonmail.com>
2023-07-08 07:35:51 +1000

    Add versioned submit block request

---
+++ spec/versionedsubmitblockrequest.go
@@ -0,0 +1,531 @@
+// Copyright © 2022 Attestant Limited.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package spec
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+
+	"github.com/attestantio/go-builder-client/api/bellatrix"
+	"github.com/attestantio/go-builder-client/api/capella"
+	consensusspec "github.com/attestantio/go-eth2-client/spec"
+	consensusbellatrix "github.com/attestantio/go-eth2-client/spec/bellatrix"
+	consensuscapella "github.com/attestantio/go-eth2-client/spec/capella"
+	"github.com/attestantio/go-eth2-client/spec/phase0"
+	"github.com/holiman/uint256"
+)
+
+// VersionedSubmitBlockRequest contains a versioned signed builder bid.
+type VersionedSubmitBlockRequest struct {
+	Version   consensusspec.DataVersion
+	Bellatrix *bellatrix.SubmitBlockRequest
+	Capella   *capella.SubmitBlockRequest
+}
+
+// IsEmpty returns true if there is no request.
+func (v *VersionedSubmitBlockRequest) IsEmpty() bool {
+	switch v.Version {
+	case consensusspec.DataVersionBellatrix:
+		return v.Bellatrix == nil
+	case consensusspec.DataVersionCapella:
+		return v.Capella == nil
+	default:
+		return true
+	}
+}
+
+// Slot returns the slot of the request
+func (v *VersionedSubmitBlockRequest) Slot() (uint64, error) {
+	if v == nil {
+		return 0, errors.New("nil struct")
+	}
+	switch v.Version {
+	case consensusspec.DataVersionBellatrix:
+		if v.Bellatrix == nil {
+			return 0, errors.New("no data")
+		}
+		if v.Bellatrix.Message == nil {
+			return 0, errors.New("no data message")
+		}
+		return v.Bellatrix.Message.Slot, nil
+	case consensusspec.DataVersionCapella:
+		if v.Capella == nil {
+			return 0, errors.New("no data")
+		}
+		if v.Capella.Message == nil {
+			return 0, errors.New("no data message")
+		}
+		return v.Capella.Message.Slot, nil
+	default:
+		return 0, errors.New("unsupported version")
+	}
+}
+
+// BlockHash returns the block hash of the request.
+func (v *VersionedSubmitBlockRequest) BlockHash() (phase0.Hash32, error) {
+	if v == nil {
+		return phase0.Hash32{}, errors.New("nil struct")
+	}
+	switch v.Version {
+	case consensusspec.DataVersionBellatrix:
+		if v.Bellatrix == nil {
+			return phase0.Hash32{}, errors.New("no data")
+		}
+		if v.Bellatrix.Message == nil {
+			return phase0.Hash32{}, errors.New("no data message")
+		}
+		return v.Bellatrix.Message.BlockHash, nil
+	case consensusspec.DataVersionCapella:
+		if v.Capella == nil {
+			return phase0.Hash32{}, errors.New("no data")
+		}
+		if v.Capella.Message == nil {
+			return phase0.Hash32{}, errors.New("no data message")
+		}
+		return v.Capella.Message.BlockHash, nil
+	default:
+		return phase0.Hash32{}, errors.New("unsupported version")
+	}
+}
+
+// Builder returns the builder of the request.
+func (v *VersionedSubmitBlockRequest) Builder() (phase0.BLSPubKey, error) {
+	if v == nil {
+		return phase0.BLSPubKey{}, errors.New("nil struct")
+	}
+	switch v.Version {
+	case consensusspec.DataVersionBellatrix:
+		if v.Bellatrix == nil {
+			return phase0.BLSPubKey{}, errors.New("no data")
+		}
+		if v.Bellatrix.Message == nil {
+			return phase0.BLSPubKey{}, errors.New("no data message")
+		}
+		return v.Bellatrix.Message.BuilderPubkey, nil
+	case consensusspec.DataVersionCapella:
+		if v.Capella == nil {
+			return phase0.BLSPubKey{}, errors.New("no data")
+		}
+		if v.Capella.Message == nil {
+			return phase0.BLSPubKey{}, errors.New("no data message")
+		}
+		return v.Capella.Message.BuilderPubkey, nil
+	default:
+		return phase0.BLSPubKey{}, errors.New("unsupported version")
+	}
+}
+
+// ProposerFeeRecipient returns the proposer fee recipient of the request.
+func (v *VersionedSubmitBlockRequest) ProposerFeeRecipient() (consensusbellatrix.ExecutionAddress, error) {
+	if v == nil {
+		return consensusbellatrix.ExecutionAddress{}, errors.New("nil struct")
+	}
+	switch v.Version {
+	case consensusspec.DataVersionBellatrix:
+		if v.Bellatrix == nil {
+			return consensusbellatrix.ExecutionAddress{}, errors.New("no data")
+		}
+		if v.Bellatrix.Message == nil {
+			return consensusbellatrix.ExecutionAddress{}, errors.New("no data message")
+		}
+		return v.Bellatrix.Message.ProposerFeeRecipient, nil
+	case consensusspec.DataVersionCapella:
+		if v.Capella == nil {
+			return consensusbellatrix.ExecutionAddress{}, errors.New("no data")
+		}
+		if v.Capella.Message == nil {
+			return consensusbellatrix.ExecutionAddress{}, errors.New("no data message")
+		}
+		return v.Capella.Message.ProposerFeeRecipient, nil
+	default:
+		return consensusbellatrix.ExecutionAddress{}, errors.New("unsupported version")
+	}
+}
+
+// ProposerPubkey returns the proposer fee recipient of the request.
+func (v *VersionedSubmitBlockRequest) ProposerPubkey() (phase0.BLSPubKey, error) {
+	if v == nil {
+		return phase0.BLSPubKey{}, errors.New("nil struct")
+	}
+	switch v.Version {
+	case consensusspec.DataVersionBellatrix:
+		if v.Bellatrix == nil {
+			return phase0.BLSPubKey{}, errors.New("no data")
+		}
+		if v.Bellatrix.Message == nil {
+			return phase0.BLSPubKey{}, errors.New("no data message")
+		}
+		return v.Bellatrix.Message.ProposerPubkey, nil
+	case consensusspec.DataVersionCapella:
+		if v.Capella == nil {
+			return phase0.BLSPubKey{}, errors.New("no data")
+		}
+		if v.Capella.Message == nil {
+			return phase0.BLSPubKey{}, errors.New("no data message")
+		}
+		return v.Capella.Message.ProposerPubkey, nil
+	default:
+		return phase0.BLSPubKey{}, errors.New("unsupported version")
+	}
+}
+
+// ParentHash returns the parent hash of the request.
+func (v *VersionedSubmitBlockRequest) ParentHash() (phase0.Hash32, error) {
+	if v == nil {
+		return phase0.Hash32{}, errors.New("nil struct")
+	}
+	switch v.Version {
+	case consensusspec.DataVersionBellatrix:
+		if v.Bellatrix == nil {
+			return phase0.Hash32{}, errors.New("no data")
+		}
+		if v.Bellatrix.Message == nil {
+			return phase0.Hash32{}, errors.New("no data message")
+		}
+		return v.Bellatrix.Message.ParentHash, nil
+	case consensusspec.DataVersionCapella:
+		if v.Capella == nil {
+			return phase0.Hash32{}, errors.New("no data")
+		}
+		if v.Capella.Message == nil {
+			return phase0.Hash32{}, errors.New("no data message")
+		}
+		return v.Capella.Message.ParentHash, nil
+	default:
+		return phase0.Hash32{}, errors.New("unsupported version")
+	}
+}
+
+// Value returns the value of the request.
+func (v *VersionedSubmitBlockRequest) Value() (*uint256.Int, error) {
+	if v == nil {
+		return nil, errors.New("nil struct")
+	}
+	switch v.Version {
+	case consensusspec.DataVersionBellatrix:
+		if v.Bellatrix == nil {
+			return nil, errors.New("no data")
+		}
+		if v.Bellatrix.Message == nil {
+			return nil, errors.New("no data message")
+		}
+		return v.Bellatrix.Message.Value, nil
+	case consensusspec.DataVersionCapella:
+		if v.Capella == nil {
+			return nil, errors.New("no data")
+		}
+		if v.Capella.Message == nil {
+			return nil, errors.New("no data message")
+		}
+		return v.Capella.Message.Value, nil
+	default:
+		return nil, errors.New("unsupported version")
+	}
+}
+
+// MessageHashTreeRoot returns the hash tree root of the message of the bid.
+func (v *VersionedSubmitBlockRequest) MessageHashTreeRoot() (phase0.Root, error) {
+	if v == nil {
+		return phase0.Root{}, errors.New("nil struct")
+	}
+	switch v.Version {
+	case consensusspec.DataVersionBellatrix:
+		if v.Bellatrix == nil {
+			return phase0.Root{}, errors.New("no data")
+		}
+		if v.Bellatrix.Message == nil {
+			return phase0.Root{}, errors.New("no data message")
+		}
+		return v.Bellatrix.Message.HashTreeRoot()
+	case consensusspec.DataVersionCapella:
+		if v.Capella == nil {
+			return phase0.Root{}, errors.New("no data")
+		}
+		if v.Capella.Message == nil {
+			return phase0.Root{}, errors.New("no data message")
+		}
+		return v.Capella.Message.HashTreeRoot()
+	default:
+		return phase0.Root{}, errors.New("unsupported version")
+	}
+}
+
+// Signature returns the signature of the bid.
+func (v *VersionedSubmitBlockRequest) Signature() (phase0.BLSSignature, error) {
+	if v == nil {
+		return phase0.BLSSignature{}, errors.New("nil struct")
+	}
+	switch v.Version {
+	case consensusspec.DataVersionBellatrix:
+		if v.Bellatrix == nil {
+			return phase0.BLSSignature{}, errors.New("no data")
+		}
+		return v.Bellatrix.Signature, nil
+	case consensusspec.DataVersionCapella:
+		if v.Capella == nil {
+			return phase0.BLSSignature{}, errors.New("no data")
+		}
+		return v.Capella.Signature, nil
+	default:
+		return phase0.BLSSignature{}, errors.New("unsupported version")
+	}
+}
+
+// ExecutionPayloadBlockHash returns the block hash of the payload
+func (v *VersionedSubmitBlockRequest) ExecutionPayloadBlockHash() (phase0.Hash32, error) {
+	if v == nil {
+		return phase0.Hash32{}, errors.New("nil struct")
+	}
+	switch v.Version {
+	case consensusspec.DataVersionBellatrix:
+		if v.Bellatrix == nil {
+			return phase0.Hash32{}, errors.New("no data")
+		}
+		if v.Bellatrix.ExecutionPayload == nil {
+			return phase0.Hash32{}, errors.New("no data execution payload")
+		}
+		return v.Bellatrix.ExecutionPayload.BlockHash, nil
+	case consensusspec.DataVersionCapella:
+		if v.Capella == nil {
+			return phase0.Hash32{}, errors.New("no data")
+		}
+		if v.Capella.ExecutionPayload == nil {
+			return phase0.Hash32{}, errors.New("no data execution payload")
+		}
+		return v.Capella.ExecutionPayload.BlockHash, nil
+	default:
+		return phase0.Hash32{}, errors.New("unsupported version")
+	}
+}
+
+// ExecutionPayloadParentHash returns the block hash of the payload
+func (v *VersionedSubmitBlockRequest) ExecutionPayloadParentHash() (phase0.Hash32, error) {
+	if v == nil {
+		return phase0.Hash32{}, errors.New("nil struct")
+	}
+	switch v.Version {
+	case consensusspec.DataVersionBellatrix:
+		if v.Bellatrix == nil {
+			return phase0.Hash32{}, errors.New("no data")
+		}
+		if v.Bellatrix.ExecutionPayload == nil {
+			return phase0.Hash32{}, errors.New("no data execution payload")
+		}
+		return v.Bellatrix.ExecutionPayload.ParentHash, nil
+	case consensusspec.DataVersionCapella:
+		if v.Capella == nil {
+			return phase0.Hash32{}, errors.New("no data")
+		}
+		if v.Capella.ExecutionPayload == nil {
+			return phase0.Hash32{}, errors.New("no data execution payload")
+		}
+		return v.Capella.ExecutionPayload.ParentHash, nil
+	default:
+		return phase0.Hash32{}, errors.New("unsupported version")
+	}
+}
+
+// PrevRandao returns the prev randao of the payload
+func (v *VersionedSubmitBlockRequest) PrevRandao() (phase0.Hash32, error) {
+	if v == nil {
+		return phase0.Hash32{}, errors.New("nil struct")
+	}
+	switch v.Version {
+	case consensusspec.DataVersionBellatrix:
+		if v.Bellatrix == nil {
+			return phase0.Hash32{}, errors.New("no data")
+		}
+		if v.Bellatrix.ExecutionPayload == nil {
+			return phase0.Hash32{}, errors.New("no data execution payload")
+		}
+		return v.Bellatrix.ExecutionPayload.PrevRandao, nil
+	case consensusspec.DataVersionCapella:
+		if v.Capella == nil {
+			return phase0.Hash32{}, errors.New("no data")
+		}
+		if v.Capella.ExecutionPayload == nil {
+			return phase0.Hash32{}, errors.New("no data execution payload")
+		}
+		return v.Capella.ExecutionPayload.PrevRandao, nil
+	default:
+		return phase0.Hash32{}, errors.New("unsupported version")
+	}
+}
+
+// GasLimit returns the prev randao of the payload
+func (v *VersionedSubmitBlockRequest) GasLimit() (uint64, error) {
+	if v == nil {
+		return 0, errors.New("nil struct")
+	}
+	switch v.Version {
+	case consensusspec.DataVersionBellatrix:
+		if v.Bellatrix == nil {
+			return 0, errors.New("no data")
+		}
+		if v.Bellatrix.ExecutionPayload == nil {
+			return 0, errors.New("no data execution payload")
+		}
+		return v.Bellatrix.ExecutionPayload.GasLimit, nil
+	case consensusspec.DataVersionCapella:
+		if v.Capella == nil {
+			return 0, errors.New("no data")
+		}
+		if v.Capella.ExecutionPayload == nil {
+			return 0, errors.New("no data execution payload")
+		}
+		return v.Capella.ExecutionPayload.GasLimit, nil
+	default:
+		return 0, errors.New("unsupported version")
+	}
+}
+
+// GasUsed returns the prev randao of the payload
+func (v *VersionedSubmitBlockRequest) GasUsed() (uint64, error) {
+	if v == nil {
+		return 0, errors.New("nil struct")
+	}
+	switch v.Version {
+	case consensusspec.DataVersionBellatrix:
+		if v.Bellatrix == nil {
+			return 0, errors.New("no data")
+		}
+		if v.Bellatrix.ExecutionPayload == nil {
+			return 0, errors.New("no data execution payload")
+		}
+		return v.Bellatrix.ExecutionPayload.GasUsed, nil
+	case consensusspec.DataVersionCapella:
+		if v.Capella == nil {
+			return 0, errors.New("no data")
+		}
+		if v.Capella.ExecutionPayload == nil {
+			return 0, errors.New("no data execution payload")
+		}
+		return v.Capella.ExecutionPayload.GasUsed, nil
+	default:
+		return 0, errors.New("unsupported version")
+	}
+}
+
+// BlockNumber returns the block number of the payload.
+func (v *VersionedSubmitBlockRequest) BlockNumber() (uint64, error) {
+	if v == nil {
+		return 0, errors.New("nil struct")
+	}
+	switch v.Version {
+	case consensusspec.DataVersionBellatrix:
+		if v.Bellatrix == nil {
+			return 0, errors.New("no data")
+		}
+		if v.Bellatrix.ExecutionPayload == nil {
+			return 0, errors.New("no data execution payload")
+		}
+		return v.Bellatrix.ExecutionPayload.BlockNumber, nil
+	case consensusspec.DataVersionCapella:
+		if v.Capella == nil {
+			return 0, errors.New("no data")
+		}
+		if v.Capella.ExecutionPayload == nil {
+			return 0, errors.New("no data execution payload")
+		}
+		return v.Capella.ExecutionPayload.BlockNumber, nil
+	default:
+		return 0, errors.New("unsupported version")
+	}
+}
+
+// Timestamp returns the timestamp of the payload.
+func (v *VersionedSubmitBlockRequest) Timestamp() (uint64, error) {
+	if v == nil {
+		return 0, errors.New("nil struct")
+	}
+	switch v.Version {
+	case consensusspec.DataVersionBellatrix:
+		if v.Bellatrix == nil {
+			return 0, errors.New("no data")
+		}
+		if v.Bellatrix.ExecutionPayload == nil {
+			return 0, errors.New("no data execution payload")
+		}
+		return v.Bellatrix.ExecutionPayload.Timestamp, nil
+	case consensusspec.DataVersionCapella:
+		if v.Capella == nil {
+			return 0, errors.New("no data")
+		}
+		if v.Capella.ExecutionPayload == nil {
+			return 0, errors.New("no data execution payload")
+		}
+		return v.Capella.ExecutionPayload.Timestamp, nil
+	default:
+		return 0, errors.New("unsupported version")
+	}
+}
+
+// Transactions returns the transactions of the payload
+func (v *VersionedSubmitBlockRequest) Transactions() ([]consensusbellatrix.Transaction, error) {
+	if v == nil {
+		return nil, errors.New("nil struct")
+	}
+	switch v.Version {
+	case consensusspec.DataVersionBellatrix:
+		if v.Bellatrix == nil {
+			return nil, errors.New("no data")
+		}
+		if v.Bellatrix.ExecutionPayload == nil {
+			return nil, errors.New("no data execution payload")
+		}
+		return v.Bellatrix.ExecutionPayload.Transactions, nil
+	case consensusspec.DataVersionCapella:
+		if v.Capella == nil {
+			return nil, errors.New("no data")
+		}
+		if v.Capella.ExecutionPayload == nil {
+			return nil, errors.New("no data execution payload")
+		}
+		return v.Capella.ExecutionPayload.Transactions, nil
+	default:
+		return nil, errors.New("unsupported version")
+	}
+}
+
+// Withdrawals returns the withdrawals of the payload
+func (v *VersionedSubmitBlockRequest) Withdrawals() ([]*consensuscapella.Withdrawal, error) {
+	if v == nil {
+		return nil, errors.New("nil struct")
+	}
+	switch v.Version {
+	case consensusspec.DataVersionCapella:
+		if v.Capella == nil {
+			return nil, errors.New("no data")
+		}
+		if v.Capella.ExecutionPayload == nil {
+			return nil, errors.New("no data execution payload")
+		}
+		return v.Capella.ExecutionPayload.Withdrawals, nil
+	default:
+		return nil, errors.New("unsupported version")
+	}
+}
+
+// String returns a string version of the structure.
+func (v *VersionedSubmitBlockRequest) String() string {
+	if v == nil {
+		return ""
+	}
+	data, err := json.Marshal(v)
+	if err != nil {
+		return fmt.Sprintf("ERR: %v", err)
+	}
+	return string(data)
+}

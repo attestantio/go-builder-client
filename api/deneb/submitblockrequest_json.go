8e193467ce21dfe9752eb6ccaa8a45b0b0a69530
avalonche <avalonche@protonmail.com>
2023-07-25 12:59:47 +1000

    Add builder bid structs for deneb

---
+++ api/deneb/submitblockrequest_json.go
@@ -0,0 +1,63 @@
+package deneb
+
+import (
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+	"strings"
+
+	v1 "github.com/attestantio/go-builder-client/api/v1"
+	"github.com/attestantio/go-eth2-client/spec/deneb"
+	"github.com/attestantio/go-eth2-client/spec/phase0"
+	"github.com/pkg/errors"
+)
+
+// submitBlockRequestJSON is the spec representation of the struct.
+type submitBlockRequestJSON struct {
+	Message          *v1.BidTrace            `json:"message"`
+	ExecutionPayload *deneb.ExecutionPayload `json:"execution_payload"`
+	Signature        string                  `json:"signature"`
+}
+
+// MarshalJSON implements json.Marshaler.
+func (s *SubmitBlockRequest) MarshalJSON() ([]byte, error) {
+	return json.Marshal(&submitBlockRequestJSON{
+		Signature:        fmt.Sprintf("%#x", s.Signature),
+		Message:          s.Message,
+		ExecutionPayload: s.ExecutionPayload,
+	})
+}
+
+// UnmarshalJSON implements json.Unmarshaler.
+func (s *SubmitBlockRequest) UnmarshalJSON(input []byte) error {
+	var data submitBlockRequestJSON
+	if err := json.Unmarshal(input, &data); err != nil {
+		return errors.Wrap(err, "invalid JSON")
+	}
+	return s.unpack(&data)
+}
+
+func (s *SubmitBlockRequest) unpack(data *submitBlockRequestJSON) error {
+	if data.Message == nil {
+		return errors.New("message missing")
+	}
+	s.Message = data.Message
+
+	if data.Signature == "" {
+		return errors.New("signature missing")
+	}
+	signature, err := hex.DecodeString(strings.TrimPrefix(data.Signature, "0x"))
+	if err != nil {
+		return errors.Wrap(err, "invalid signature")
+	}
+	if len(signature) != phase0.SignatureLength {
+		return errors.New("incorrect length for signature")
+	}
+	copy(s.Signature[:], signature)
+
+	if data.ExecutionPayload == nil {
+		return errors.New("execution payload missing")
+	}
+	s.ExecutionPayload = data.ExecutionPayload
+	return nil
+}
---
+++ api/deneb/submitblockrequest_yaml.go
@@ -0,0 +1,40 @@
+package deneb
+
+import (
+	"bytes"
+	"fmt"
+
+	v1 "github.com/attestantio/go-builder-client/api/v1"
+	"github.com/attestantio/go-eth2-client/spec/deneb"
+	"github.com/goccy/go-yaml"
+)
+
+// submitBlockRequestYAML is the spec representation of the struct.
+type submitBlockRequestYAML struct {
+	Message          *v1.BidTrace            `yaml:"message"`
+	ExecutionPayload *deneb.ExecutionPayload `yaml:"execution_payload"`
+	Signature        string                  `yaml:"signature"`
+}
+
+// MarshalYAML implements yaml.Marshaler.
+func (s *SubmitBlockRequest) MarshalYAML() ([]byte, error) {
+	yamlBytes, err := yaml.MarshalWithOptions(&submitBlockRequestYAML{
+		Message:          s.Message,
+		Signature:        fmt.Sprintf("%#x", s.Signature),
+		ExecutionPayload: s.ExecutionPayload,
+	}, yaml.Flow(true))
+	if err != nil {
+		return nil, err
+	}
+	return bytes.ReplaceAll(yamlBytes, []byte(`"`), []byte(`'`)), nil
+}
+
+// UnmarshalYAML implements yaml.Unmarshaler.
+func (s *SubmitBlockRequest) UnmarshalYAML(input []byte) error {
+	// We unmarshal to the JSON struct to save on duplicate code.
+	var data submitBlockRequestJSON
+	if err := yaml.Unmarshal(input, &data); err != nil {
+		return err
+	}
+	return s.unpack(&data)
+}

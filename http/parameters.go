619a7d804d461c1253097618cbc138663755e908
Chris Berry <bez625@gmail.com>
2026-07-17 14:06:00 +0100

    Update linter settings.

--- http/parameters.go
+++ http/parameters.go
@@ -11,6 +11,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+//nolint:revive,nolintlint // package name is the same as std lib; keep public API.
 package http
 
 import (
